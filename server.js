2c5fc26d5f410020c6e3d4ec2c4b5c785b4efa60
VictorRaffaele <victorraffaele@gmail.com>
2025-07-02 17:56:12 -0300

    feat: inicializa bot com o serviço e chama rotas da api

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
-const port = process.env.NODE_PORT;
+const { startBot } = require('./src/services/bot');
+const { router } = require('./src/routers/index');
 
 dotenv.config();
+const port = process.env.NODE_PORT;
+
 // Configurando o middleware para processar dados de formulários e JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Importando as rotas
+app.use('/api', router(express));
 // Servindo arquivos estáticos da pasta "public"
 app.use('/', express.static("public"));
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
+  // Inicia o bot WhatsApp junto com o servidor
+  startBot();
 });
