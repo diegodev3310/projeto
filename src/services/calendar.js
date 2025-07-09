70e19dbb007e9a5de44aee1b520156dfc5f7ba94
VictorRaffaele <victorraffaele@gmail.com>
2025-07-09 17:08:08 -0300

    feat: config email do negocio como env

--- src/services/calendar.js
+++ src/services/calendar.js
@@ -2,7 +2,6 @@ const { google } = require('googleapis');
 const dotenv = require('dotenv');
 const path = require('path');
 
-const funcTag = '[calendar]';
 dotenv.config(path.resolve(__dirname, '../../.env'));
 
 function setClient() {
@@ -20,33 +19,33 @@ function setClient() {
   return oauth2Client;
 }
 
-
-async function createEvent(emailPrestador, clientEmail, dataHora) {
-  console.log(`${funcTag} Iniciando criação de evento no Google Calendar...`);
-  const oauth2Client = setClient();
-  const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
-
-  const evento = {
-    summary: 'Consulta Agendada',
-    location: 'Endereço ou link do meeting',
-    description: 'Agendamento via sistema automatizado',
-    start: { dateTime: dataHora.inicio, timeZone: 'America/Sao_Paulo' },
-    end: { dateTime: dataHora.fim, timeZone: 'America/Sao_Paulo' },
-    attendees: [
-      { email: clientEmail },
-      { email: emailPrestador }
-    ],
-    sendUpdates: 'all',
-    reminders: {
-      useDefault: false,
-      overrides: [
-        { method: 'email', minutes: 24 * 60 }, // 1 dia antes
-        { method: 'popup', minutes: 60 }, // 60 minutos antes
-      ],
-    },
-  };
-
+async function createEvent(clientEmail, dataHora) {
+  const funcTag = '[createEvent]';
   try {
+    console.log(`${funcTag} Iniciando criação de evento no Google Calendar...`);
+    const oauth2Client = setClient();
+    const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
+    const businessEmail = process.env.BUSINESS_EMAIL;
+    const evento = {
+      summary: 'Consulta Agendada',
+      location: 'Endereço ou link do meeting',
+      description: 'Agendamento via sistema automatizado',
+      start: { dateTime: dataHora.inicio, timeZone: 'America/Sao_Paulo' },
+      end: { dateTime: dataHora.fim, timeZone: 'America/Sao_Paulo' },
+      attendees: [
+        { email: clientEmail },
+        { email: businessEmail }
+      ],
+      sendUpdates: 'all',
+      reminders: {
+        useDefault: false,
+        overrides: [
+          { method: 'email', minutes: 24 * 60 }, // 1 dia antes
+          { method: 'popup', minutes: 60 }, // 60 minutos antes
+        ],
+      },
+    };
+
     console.log(`${funcTag} Criando evento`)
     const res = await calendar.events.insert({
       calendarId: 'primary',
@@ -56,12 +55,11 @@ async function createEvent(emailPrestador, clientEmail, dataHora) {
     return res.data.htmlLink;
   } catch (err) {
     console.error(`${funcTag} Erro ao criar evento:`, err);
-    throw new Error(`Erro ao criar evento: ${err.message}`);
   }
 }
 
 // Exemplo de chamada
-// createEvent('test@gmail.com', 'client@gmail.com', {
+// createEvent('client@gmail.com', {
 //   inicio: '2025-07-10T10:00:00-03:00',
 //   fim: '2025-07-10T11:00:00-03:00'
 // });
