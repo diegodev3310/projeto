5c3349c498258597ee7b44ba44b48c3a83903d68
VictorRaffaele <victorraffaele@gmail.com>
2025-07-10 18:05:12 -0300

    feat: rota de criação de mensagens

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,11 +1,15 @@
 const { BotController } = require('../controllers/bot');
+const { MesssageBotController } = require('../controllers/messagesBotController');
 
 function router(express) {
   const router = express.Router();
   const botCtrl = new BotController();
+  const messagesBotCtrl = new MesssageBotController();
 
   router.get('/generate-qr', botCtrl.getQrCode);
   router.get('/status', botCtrl.getClientStatus);
+  
+  router.post('/messages', messagesBotCtrl.create);
 
   return router;
 }
