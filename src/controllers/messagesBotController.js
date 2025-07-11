c441a7d913538b7fa3695a83a0274dcade923627
VictorRaffaele <victorraffaele@gmail.com>
2025-07-11 16:04:34 -0300

    feat: Controller pra chamada via API

--- src/controllers/messagesBotController.js
+++ src/controllers/messagesBotController.js
@@ -16,6 +16,17 @@ class MesssageBotController {
   }
 
   async readAll(req, res) {
+    const funcTag = '[MesssageBotController.readAll]';
+    try {
+      const messagesBotService = new MessagesBotService();
+      console.log(`${funcTag} Iniciando recuperação de mensagens...`);
+      const result = await messagesBotService.readAll();
+      console.log(`${funcTag} Mensagem recuperadas com sucesso:`, result);
+      res.status(result.status).json(result);
+    } catch (error) {
+      console.error(`${funcTag} Erro ao recuperar mensagem:`, error);
+      res.status(500).json({ error: 'Erro ao recuperar mensagem' });
+    }
   }
 
   async update(req, res) {
