3db20fafc4868fe02f1aa318ff14f9cf0cef7bb5
VictorRaffaele <victorraffaele@gmail.com>
2025-07-11 14:01:47 -0300

    feat: função para reuso de validar msg

--- src/services/messagesBotService.js
+++ src/services/messagesBotService.js
@@ -7,17 +7,12 @@ class MessagesBotService {
     this.messagesBotRepository = new MessagesBotRepository();
   }
 
-  async create(req) {
+  async create(messageReq) {
     const funcTag = "[MessagesBotService.create]";
     try {
-      console.log(`${funcTag} Verificando dados recebidos`);
-      if (!req.message) {
-        console.error(`${funcTag} Mensagem inválida:`, req);
-        throw new Error('Mensagem inválida');
-      }
+      validadeMsg(messageReq)
       console.log(`${funcTag} Enviando dados para o repositorio`);
-      const messageRequest = new MessageRequest(req.message);
-      const result = await this.messagesBotRepository.insert(messageRequest.message);
+      const result = await this.messagesBotRepository.insert(messageReq);
       console.log(`${funcTag} Mensagem criada no repositorio`);
       const apiResponse = new ApiResponse(201, 'Mensagem criada com sucesso', result);
       return apiResponse;
@@ -43,6 +38,15 @@ class MessagesBotService {
 
   async update(id, message) {}
   async delete(id) {}
+}
+
+function validadeMsg(messageReq){
+  const funcTag = '[validadeMsg]';
+  console.log(`${funcTag} Verificando dados recebidos`);
+  if (!messageReq.message) {
+    console.error(`${funcTag} Mensagem requerida:`, messageReq);
+    throw new Error('Mensagem requerida');
+  }
 }
 
 module.exports = { MessagesBotService };
