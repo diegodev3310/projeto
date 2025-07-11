13e880bb1f3147fb1712aa78ecbe322a63969b07
VictorRaffaele <victorraffaele@gmail.com>
2025-07-11 15:35:56 -0300

    fix: conexão com DB

--- src/repository/database.js
+++ src/repository/database.js
@@ -5,24 +5,31 @@ const path = require('path');
 dotenv.config(path.resolve(__dirname, '../../.env'));
 
 class Database {
-    static #pool = null;
-    static async #init() {
-        const poolConfig = { max: process.env.PG_MAX || 3 };
-        const pool = new pg.Pool(poolConfig);
-        const res = await pool.query("SELECT VERSION()");
-        console.table(res.rows);
-        return pool;
-    }
+  static #pool = null;
+  static async #init() {
+    const poolConfig = { 
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      host:  process.env.PG_HOST || 'localhost',
+      port: process.env.PG_PORT || 5432,
+      database: process.env.PG_DB,
+      max: process.env.PG_MAX
+    };
+    const pool = new pg.Pool(poolConfig);
+    const res = await pool.query("SELECT VERSION()");
+    console.table(res.rows);
+    return pool;
+  }
 
-    static async connect() {
-        const funcTag = '[Database.connect]';
-        console.log(`${funcTag} Conectando ao banco de dados...`);
-        if (this.#pool == null) {
-            this.#pool = await this.#init();
-        }
-        console.log(`${funcTag} Conexão estabelecida com sucesso!`);
-        return this.#pool;
+  static async connect() {
+    const funcTag = '[Database.connect]';
+    console.log(`${funcTag} Conectando ao banco de dados...`);
+    if (this.#pool == null) {
+        this.#pool = await this.#init();
     }
+    console.log(`${funcTag} Conexão estabelecida com sucesso!`);
+    return this.#pool;
+  }
 }
 
 module.exports = { Database };
