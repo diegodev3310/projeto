acd3a489702104810a9fe665dffa0414a95986d1
VictorRaffaele <victorraffaele@gmail.com>
2025-08-05 15:22:33 -0300

    feat: novas rotas

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,8 @@
 const { BotController } = require('../controllers/bot');
 const { MessageBotController } = require('../controllers/messagesBotController');
 const { MessagesActionsController } = require('../controllers/messagesActionsController');
+const { MessageTransitionsController } = require('../controllers/messagesTransitionsController');
+const { ProductsController } = require('../controllers/productsController');
 const { externalAccessAuth } = require('../middleware/externalAccessAuth');
 
 function router(express) {
@@ -8,19 +10,30 @@ function router(express) {
   const botCtrl = new BotController();
   const messagesBotCtrl = new MessageBotController();
   const actionsCtrl = new MessagesActionsController();
+  const productsCtrl = new ProductsController();
+  const messagesTransitionsCtrl = new MessageTransitionsController();
 
   router.get('/actions', actionsCtrl.readAll.bind(actionsCtrl));
   router.post('/actions', externalAccessAuth, actionsCtrl.create.bind(actionsCtrl));
   router.put('/actions/:id', externalAccessAuth, actionsCtrl.update.bind(actionsCtrl));
   router.delete('/actions/:id', externalAccessAuth, actionsCtrl.delete.bind(actionsCtrl));
 
-
   router.get('/generate-qr', botCtrl.getQrCode.bind(botCtrl));
   router.get('/status', botCtrl.getClientStatus.bind(botCtrl));
 
+  router.get('/product/:productCode/:quantityCode', productsCtrl.readProd.bind(productsCtrl));
+  router.post('/product', externalAccessAuth, productsCtrl.createProd.bind(productsCtrl));
+  router.put('/product/:id', externalAccessAuth, productsCtrl.updateProd.bind(productsCtrl));
+  router.delete('/product/:id', externalAccessAuth, productsCtrl.deleteProd.bind(productsCtrl));
+
+  router.get('/transitions', messagesTransitionsCtrl.readAll.bind(messagesTransitionsCtrl));
+  router.post('/transitions', externalAccessAuth, messagesTransitionsCtrl.create.bind(messagesTransitionsCtrl));
+  router.put('/transitions', externalAccessAuth, messagesTransitionsCtrl.update.bind(messagesTransitionsCtrl));
+  router.delete('/transitions/:id', externalAccessAuth, messagesTransitionsCtrl.delete.bind(messagesTransitionsCtrl));
+
   router.get('/messages', messagesBotCtrl.readAll.bind(messagesBotCtrl));
   router.post('/messages', externalAccessAuth, messagesBotCtrl.create.bind(messagesBotCtrl));
-  router.put('/messages/:id', externalAccessAuth, messagesBotCtrl.update.bind(messagesBotCtrl));
+  router.put('/messages/:id', externalAccessAuth, messagesBotCtrl.updateMessage.bind(messagesBotCtrl));
   router.delete('/messages/:id', externalAccessAuth, messagesBotCtrl.delete.bind(messagesBotCtrl));
 
   return router;
